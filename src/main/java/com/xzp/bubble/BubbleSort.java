cb8cb91385aaecb342c0007fefbc3adee91a2bc5
徐志鹏 <xzp1164848360@126.com>
2020-11-22 21:11:29 +0800

    bubblesort

---
+++ src/main/java/com/xzp/bubble/BubbleSort.java
@@ -0,0 +1,47 @@
+package com.xzp.bubble;
+
+import com.xzp.util.Util;
+
+/**
+ * 冒泡排序
+ * @author xzp
+ * @date 2020.11.22 21:02
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int testTimes = 100_0000;//循环次数，生成多少个随机数组，校验是否都排序正确
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for(int i = 0; i < testTimes; i++){
+            int[] arr = Util.generateRandomArray(maxSize,maxValue);
+            int[] copyArr = Util.copyArray(arr);
+            sort(arr);
+            Util.rightSort(copyArr);
+            if(!Util.isEqual(arr,copyArr)){
+                succeed = false;
+                Util.print(arr);
+                Util.print(copyArr);
+                break;
+            }
+        }
+        System.out.println(succeed ? "正确" : "错误");
+
+        int[] arr = Util.generateRandomArray(10,maxValue);
+        int[] copyArr = Util.copyArray(arr);
+        Util.print(arr);
+        sort(arr);
+        Util.rightSort(copyArr);
+        Util.print(arr);
+        Util.print(copyArr);
+    }
+
+    public static void sort(int[] arr){
+        if(arr == null || arr.length < 2) return;
+        for(int i = arr.length - 1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                if(arr[j] > arr[j+1]) Util.swap(arr,j,j+1);
+            }
+        }
+    }
+}
