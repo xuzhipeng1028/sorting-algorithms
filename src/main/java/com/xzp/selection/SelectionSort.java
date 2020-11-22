16eae2a35a69266ec5f9dad17c876df66536f86e
徐志鹏 <xzp1164848360@126.com>
2020-11-22 11:40:10 +0800

    SelectionSort

--- src/main/java/com/xzp/selection/SelectionSort.java
+++ src/main/java/com/xzp/selection/SelectionSort.java
@@ -43,13 +43,12 @@ public class SelectionSort {
 
     public static void sort(int[] arr){
         if(arr == null || arr.length < 2) return;
-        int minPos;
         for(int i = 0; i < arr.length - 1; i++){
-            minPos = i;
+            int minPos = i;
             for(int j = i + 1; j < arr.length; j++){
-                if(arr[j] < arr[minPos]) minPos = j;
+                minPos = arr[j] < arr[minPos] ? j : minPos;
             }
-            if(i != minPos) Util.swap(arr,i,minPos);
+            Util.swap(arr,i,minPos);
         }
     }
 }
