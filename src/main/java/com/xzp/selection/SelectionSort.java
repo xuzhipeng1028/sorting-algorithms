9c72152f6766034f88f14a631d8f6adf9b4ff3e4
徐志鹏 <xzp1164848360@126.com>
2020-11-22 10:41:52 +0800

    SelectionSort validator

--- src/main/java/com/xzp/selection/SelectionSort.java
+++ src/main/java/com/xzp/selection/SelectionSort.java
@@ -11,12 +11,38 @@ import com.xzp.util.Util;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int[] arr = {5,1,7,3,9,4,6,10};
-        sort(arr);
+//        int[] arr = {5,1,7,3,9,4,6,10};
+//        sort(arr);
+//        Util.print(arr);
+        int testTimes = 100_0000;//循环次数，生成多少个随机数组，校验是否都排序正确
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for(int i = 0; i < testTimes; i++){
+            int[] arr = Util.generateRandomArray(maxSize,maxValue);
+            int[] copyArr = Util.copyArray(arr);
+            sort(arr);
+            Util.rightSort(copyArr);
+            if(!Util.isEqual(arr,copyArr)){
+                succeed = false;
+                Util.print(arr);
+                Util.print(copyArr);
+                break;
+            }
+        }
+        System.out.println(succeed ? "正确" : "错误");
+
+        int[] arr = Util.generateRandomArray(10,maxValue);
+        int[] copyArr = Util.copyArray(arr);
         Util.print(arr);
+        sort(arr);
+        Util.rightSort(copyArr);
+        Util.print(arr);
+        Util.print(copyArr);
     }
 
     public static void sort(int[] arr){
+        if(arr == null || arr.length < 2) return;
         int minPos;
         for(int i = 0; i < arr.length - 1; i++){
             minPos = i;
--- src/main/java/com/xzp/util/Util.java
+++ src/main/java/com/xzp/util/Util.java
@@ -1,5 +1,7 @@
 package com.xzp.util;
 
+import java.util.Arrays;
+
 /**
  * 工具类
  * @author xzp
@@ -28,5 +30,57 @@ public final class Util {
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
+    }
+
+    /**
+     * 正确排序
+     * @param arr
+     */
+    public static void rightSort(int[] arr){
+        Arrays.sort(arr);
+    }
+
+    /**
+     * 生成随机数组
+     * @param maxSize 数组最大size
+     * @param maxValue 数组元素最大值,生成[0,maxValue]范围的整数
+     * @return
+     */
+    public static int[] generateRandomArray(int maxSize,int maxValue){
+        int[] arr = new int[(int)(maxSize * Math.random() + 1)];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (int)((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    /**
+     * 拷贝原数组，生成新数组
+     * @param arr
+     * @return
+     */
+    public static int[] copyArray(int[] arr){
+        return Arrays.copyOf(arr,arr.length);
+    }
+
+    /**
+     * 判断两个数组是否相等
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static boolean isEqual(int[] arr1, int[] arr2){
+        // 如果都为null
+        if(arr1 == null && arr2 == null) return true;
+        // 如果有且仅有一个为null
+        if((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) return false;
+        // 如果都不为null,判断长度是否相等
+        if(arr1.length != arr2.length) return false;
+        //长度相等，挨个比较
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
     }
 }
